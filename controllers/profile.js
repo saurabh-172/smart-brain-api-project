e9c232fe66bdfe36076a6e9927115443bac046db
saurabh-172 <saurabh300.gole@gmail.com>
2024-10-14 11:11:48 +0530

    solving profile issues

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,18 +1,37 @@
 export const handleProfileUpdate = (req,res,db) =>{
     const {email, username, avatar, id } = req.body;
-    db('users').where('id','=',id)
-    .update({
-        email: email,
-        username: username,
-        avatar: avatar
+    db.transactions(trx => {
+        trx('login')
+        .where('id',id)
+        .update({ email })
+        .then(()=>{
+            return trx('users')
+            .where('id',id)
+            .update({ email, username, avatar })
+            .returning('*')
+        }).then(user=>{
+            if(user[0].id){
+                return res.json(user[0])
+            }else{
+                res.status(400).json('unable to update')
+            }
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
     })
-    .returning('*')
-    .then(user =>{
-        if(user[0].id){
-            res.json(user[0])
-        }else{
-            res.status(400).json('unable to update')
-        }
-    })
-    .catch(err=> res.status(400).json('unable to get update'))
+    //     ('users').where('id','=',id)
+    // .update({
+    //     email: email,
+    //     username: username,
+    //     avatar: avatar
+    // })
+    // .returning('*')
+    // .then(user =>{
+    //     if(user[0].id){
+    //         res.json(user[0])
+    //     }else{
+    //         res.status(400).json('unable to update')
+    //     }
+    // })
+    // .catch(err=> res.status(400).json('unable to get update'))
 }
