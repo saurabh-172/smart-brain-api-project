2992a2fc27546f95921bee8a6a6d4ee0a67de026
saurabh-172 <saurabh300.gole@gmail.com>
2024-10-14 10:29:03 +0530

    adding database connection string

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ import { handleImage, handleApiCall } from './controllers/image.js';
 const db = knex({
     client:'pg',
     connection: {
+        connectionString: process.env.DB_URL,
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
+        database: process.env.DB_DATABASE,
+        port: process.env.DB_PORT
     }
 });
 
